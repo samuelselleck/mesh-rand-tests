22d3ad6de1cb8fad048a63955b853f747e0a2f81
Samuel Selleck <sellecksamuel@gmail.com>
2023-03-09 01:18:41 +0100

    loaded 3d models

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,27 @@
+#![feature(iter_array_chunks)]
 use std::f64::consts::PI;
 
 use indicatif::ProgressIterator;
 use mesh_rand::{SurfSample, UniformSurface};
 use rand::distributions::Distribution;
 
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 use plotters::prelude::*;
-const OUT_FILE_NAME: &'static str = "3d-plot.gif";
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Verticies and faces for a non-regular tetrahedron:
-    let verticies = [
-        [0.0, 0.0, 0.0],
-        [1.0, 0.0, 0.0],
-        [0.0, 1.0, 0.0],
-        [0.0, 0.0, 1.0],
-    ];
-
-    let faces = [[1, 0, 2], [2, 0, 3], [0, 1, 3], [1, 2, 3]];
+const OUT_FILE_NAME: &str = "3d-plot.gif";
+fn main() -> Result<()> {
+    let (verticies, faces) = load_model("models/al.obj")?;
+    let [(sx, bx), (sy, by), (sz, bz)] = get_bounds(&verticies);
+    let (cx, cy, cz) = ((bx + sx) / 2.0, (by + sy) / 2.0, (bz + sz) / 2.0);
+    let max_dim = (bx - sx).max(by - sy).max(bz - sz) / 2.0;
+    let x_axis = ((cx - max_dim)..(cx + max_dim)).step(max_dim / 10.0);
+    let y_axis = ((cy - max_dim)..(cy + max_dim)).step(max_dim / 10.0);
+    let z_axis = ((cz - max_dim)..(cz + max_dim)).step(max_dim / 10.0);
     let mesh_dist = UniformSurface::new(&verticies, &faces)?;
     let mut rng = rand::thread_rng();
 
-    let points: Vec<_> = (0..300)
-        .into_iter()
+    println!("verts: {}, faces: {}", verticies.len(), faces.len());
+
+    let points: Vec<_> = (0..3000)
         .map(|_| {
             let SurfSample { position: p, .. } = mesh_dist.sample(&mut rng);
             (p[0] as f64, p[1] as f64, p[2] as f64)
@@ -31,13 +32,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     for i in (0..360).progress() {
         area.fill(&WHITE)?;
-
-        let x_axis = (-1.0..2.0).step(0.1);
-        let y_axis = (-1.0..2.0).step(0.1);
-        let z_axis = (-1.0..2.0).step(0.1);
-
         let mut chart = ChartBuilder::on(&area)
-            .caption(format!("3D Plot Test"), ("sans", 20))
+            .caption("mesh-rand test", ("sans", 20))
             .build_cartesian_3d(x_axis.clone(), y_axis.clone(), z_axis.clone())?;
 
         chart.with_projection(|mut pb| {
@@ -63,10 +59,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 Rectangle::new([(x + 5, y - 5), (x + 15, y + 5)], BLUE.mix(0.5).filled())
             });
 
-        chart
-            .configure_series_labels()
-            .border_style(&BLACK)
-            .draw()?;
+        chart.configure_series_labels().border_style(BLACK).draw()?;
 
         // To avoid the IO failure being ignored silently, we manually call the present function
         area.present().expect("Unable to write result to file, please make sure 'plotters-doc-data' dir exists under current dir");
@@ -74,7 +67,60 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Result has been saved to {}", OUT_FILE_NAME);
     Ok(())
 }
-#[test]
-fn entry_point() {
-    main().unwrap()
+
+fn get_bounds(verts: &[[f32; 3]]) -> [(f64, f64); 3] {
+    let mut bounds = [(f64::MAX, f64::MIN); 3];
+    for i in [0, 1, 2] {
+        for v in verts {
+            bounds[i].0 = bounds[i].0.min(v[i] as f64);
+            bounds[i].1 = bounds[i].1.max(v[i] as f64);
+        }
+    }
+    bounds
+}
+
+type Verts = Vec<[f32; 3]>;
+type Faces = Vec<[usize; 3]>;
+
+fn load_model(src: &str) -> Result<(Verts, Faces)> {
+    let (models, _) = tobj::load_obj(
+        src,
+        &tobj::LoadOptions {
+            single_index: false, //nah
+            triangulate: true,   //yes please
+            ignore_points: true,
+            ignore_lines: true,
+        },
+    )?;
+
+    let mut verticies = Vec::new();
+    let mut triangles = Vec::new();
+    for tobj::Model { mesh, .. } in models {
+        let offset = verticies.len();
+        let verts = mesh.positions.iter().copied().array_chunks();
+        let tris = mesh
+            .indices
+            .iter()
+            .map(|&v| v as usize + offset)
+            .array_chunks();
+        verticies.extend(verts);
+        triangles.extend(tris);
+    }
+
+    Ok((verticies, triangles))
+}
+
+#[allow(dead_code)]
+fn load_example() -> Result<(Verts, Faces)> {
+    // Verticies and faces for a non-regular tetrahedron:
+    let verticies = [
+        [0.0, 0.0, 0.0],
+        [1.0, 0.0, 0.0],
+        [0.0, 1.0, 0.0],
+        [0.0, 0.0, 1.0],
+    ]
+    .into();
+
+    let faces = [[1, 0, 2], [2, 0, 3], [0, 1, 3], [1, 2, 3]].into();
+    Ok((verticies, faces))
 }
